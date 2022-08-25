39bd0d0e13cf0bfe69055e10dcd53c5a23b6209b
Alfredo Fisher <alfredo.fisher44@gmail.com>
2022-08-25 14:46:42 -0300

    Basic functions completed
    Created prompt function that allows the use to choose the grid size (howBig func). Added a few variables to make it work, and to customize divMaker func.
    
    Missing crazy mode, that turns grid blocks into random colors.

--- etcha.js
+++ etcha.js
@@ -1,11 +1,25 @@
+let bigness = 16;
 
-
-function divMaker() {
-    for (let i = 0; i < 16*16; i++){
+function divMaker(bigness) {
+    for (let i = 0; i < bigness ** 2; i++){
         let block = document.createElement('div');
         document.getElementById('container').appendChild(block);
         block.addEventListener('mouseover',() => block.style.background = 'black');
     }
 }
 
-divMaker();
+
+divMaker(bigness);
+let container = document.getElementById('container')
+let button = document.getElementById('button').addEventListener('click', function howBig (){
+    bigness = prompt('How big?', '16, max 100');
+    if (bigness > 100 || isNaN(bigness)) howBig();
+    
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    container.style.gridTemplateColumns = `repeat(${bigness}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${bigness}, 1fr)`
+    divMaker(bigness);
+}
+)
